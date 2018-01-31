68a21c7b38d3845fd8d9b625c39fed52c60930c6
The Powershell Bear <poshcodebear@outlook.com>
2018-01-30 22:36:54 -0800

    Added Cell class

---
+++ TicTacToe/Cell.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TicTacToe
+{
+    class Cell
+    {
+        int row;
+        int col;
+        Owner selection;
+
+        public int Row { get => row; }
+        public int Col { get => col; }
+        public Owner Selection { get => selection; }
+
+        Cell(int Row, int Col)
+        {
+            row = Row;
+            col = Col;
+            selection = Owner.None;
+        }
+
+        void SetOwner(Owner selection)
+        {
+            if (this.selection == Owner.None)
+                this.selection = selection;
+        }
+    }
+
+    enum Owner
+    {
+        X, Y, None
+    }
+}
